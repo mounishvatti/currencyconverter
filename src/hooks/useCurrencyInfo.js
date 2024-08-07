218867db11c144968de624f4599c9ae50ac1cb2e
mounishvatti <mounishvatti2002@gmail.com>
2024-08-07 21:19:46 +0530

    minor upt

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react'
 
-function useCurrencyInfo(currency) { // designing a custom hook 
+function useCurrencyInfo(currency) { // designing a custom hook to fetch currency data from external API
     const [data, setData] = useState({})
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
     }, [currency])
-    
+
     return data
 }
 
